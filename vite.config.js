58201692eaa4d7e0f41c48c1d24318c0a2cfd350
ameerhamza1490 <146176047+ameerhamza1490@users.noreply.github.com>
2025-06-30 16:01:34 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite"
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/Fake-Ecommerce-Store" || "/"
-})
+  base: process.env.VITE_BASE_PATH || "/",
+  // "/Fake-Ecommerce-Store"
+});
