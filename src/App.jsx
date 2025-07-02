55a8cf5d48a90078dcd7d0e624e99da189c31573
ameerhamza1490 <146176047+ameerhamza1490@users.noreply.github.com>
2025-07-02 05:58:55 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import "./App.css";
 import AppBar from "./components/Appbar";
 import Products from "./pages/Products";
 import Home from "./pages/Home";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ProductDetail from "./pages/ProductDetail";
+import Contact from "./pages/Contact";
 const App = () => {
   return (
     <>
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/products/">
           <Route index element={<Products />} />
           <Route path="productdetail/:id" element={<ProductDetail />} />
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "../App.css";
 
 const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+  const [product, setProduct] = useState({});
+
+  const { id } = useParams();
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+    setProduct(response.data);
+  };
+  console.log(product);
+  return (
+    <>
+      <div className="p-6 flex items-center justify-center">
+        <div className="p-4 bg-white flex flex-col lg:flex-row justify-around items-center gap-6 rounded-2xl">
+          <img src={product.image} alt="" className="w-96" />
+          <div className="flex flex-col gap-4">
+            <h1 className="text-4xl font-extrabold">{product.title}</h1>
+            <p>{product.description}</p>
+            <p className="text-2xl font-bold">${product.price}</p>
+            <div>
+              <button className="p-4 bg-blue-600 active:hover:bg-blue-700 hover:bg-blue-700 text-white font-semibold py-2 rounded-xl transition duration-200 ease-in-out cursor-pointer">
+                Buy Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProductDetail;
